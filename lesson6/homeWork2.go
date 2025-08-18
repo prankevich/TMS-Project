2e3cfa3a005f9821e6f4fd64896c7efa6679429b
Prankevich <prankevich.alexandr@gmail.com>
2025-08-18 12:46:28 +0300

    adding new functions in homeWork2   and trying to  implementing the interface

--- lesson6/homeWork2.go
+++ lesson6/homeWork2.go
@@ -16,34 +16,37 @@ type Printable interface {
 }
 
 func main() {
-	emp := []Employee{
-		{Name: "Alex", Age: 20, Salary: 1000, Position: "developer"}}
-	emp = addingEmployee(emp, "Sveta", 25, 1500, "manager")
-	fmt.Println(emp)
-	sal := make(map[string]float64)
-	for _, e := range emp {
-		sal[e.Position] = e.Salary
-		fmt.Println(sal["manager"])
+
+	person1 := Employee{"Gleb", 27, "baker", 2700}
+	person2 := Employee{"Sveta", 27, "baker", 1700}
+	person3 := Employee{"Alex", 20, "developer", 1000}
+
+	var emp []Employee
+	emp = addingEmployee(emp, person1)
+	emp = addingEmployee(emp, person2)
+	emp = addingEmployee(emp, person3)
+
+	avgmap := averageSalary(emp)
+	fmt.Println("Вывод обновленных данных о средней З/П по должности  :", avgmap["baker"])
+	fmt.Printf("Средний возраст  всех сотрудников: %2.f \n", averageAge(emp))
+	result, err := searchEmployee(emp, "Alex")
+	if err != nil {
+		fmt.Println(err)
 	}
-	var p Printable = emp[1]
-	p.Print()
-	fmt.Println(searchEmployee(emp, "Sveta"))
+	for _, e := range result {
+		var p Printable = e
+		p.Print()
+	}
+
 }
 
 func (e Employee) Info() string {
-	return fmt.Sprint(e.Name, ", ", e.Age, ", ", e.Position, ", ", e.Salary)
+	return fmt.Sprintf("Имя:%-10s Возраст:%-3d Должность:%-10s Зарплата:%.2f", e.Name, e.Age, e.Position, e.Salary)
 
 }
-
-func addingEmployee(emp []Employee, n string, a int, s float64, p string) []Employee {
-	newEmp := Employee{
-		Name:     n,
-		Age:      a,
-		Salary:   s,
-		Position: p,
-	}
-	return append(emp, newEmp)
-
+func addingEmployee(emp []Employee, person Employee) []Employee {
+	newEmp := append(emp, person)
+	return newEmp
 }
 func (e Employee) Print() {
 	fmt.Println(e.Info())
@@ -54,10 +57,33 @@ func searchEmployee(emp []Employee, name string) ([]Employee, error) {
 	for _, e := range emp {
 		if e.Name == name {
 			resultSearch = append(resultSearch, e)
-		} else {
-			return nil, errors.New("Нет имени ")
 		}
 	}
+	if len(resultSearch) == 0 {
+		return nil, errors.New("Нет такого имени")
+	}
 	return resultSearch, nil
+}
+func averageAge(emp []Employee) float64 {
+	var sum float64
+	for _, e := range emp {
+		sum += float64(e.Age)
+	}
+	return sum / float64(len(emp))
 
+}
+func averageSalary(emp []Employee) map[string]float64 {
+	countSum := make(map[string]float64)
+	countPers := make(map[string]int)
+	for _, e := range emp {
+		countSum[e.Position] += e.Salary
+		countPers[e.Position]++
+	}
+
+	average := make(map[string]float64)
+	for p, s := range countSum {
+		average[p] = s / float64(countPers[p])
+
+	}
+	return average
 }
