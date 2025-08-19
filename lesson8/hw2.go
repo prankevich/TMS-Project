6ed1c715feb85d7614ed5ef75d6a8491040cd4d6
Prankevich <prankevich.alexandr@gmail.com>
2025-08-19 10:49:43 +0300

    Added 2 point of task

---
+++ lesson8/hw2.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	var wg sync.WaitGroup
+
+	t := time.Now()
+	wg.Add(1)
+	go uploadedFile3(&wg)
+	wg.Add(1)
+	go uploadedFile2(&wg)
+	wg.Add(1)
+	go uploadedFile1(&wg)
+
+	wg.Wait()
+	fmt.Println(time.Since(t))
+}
+func uploadedFile1(wg *sync.WaitGroup) {
+	time.Sleep(1 * time.Second)
+	fmt.Println("file1 loaded")
+	wg.Done()
+}
+func uploadedFile2(wg *sync.WaitGroup) {
+	time.Sleep(2 * time.Second)
+	fmt.Println("file1 loaded")
+	wg.Done()
+
+}
+func uploadedFile3(wg *sync.WaitGroup) {
+	time.Sleep(500 * time.Millisecond)
+	fmt.Println("file1 loaded")
+	wg.Done()
+
+}
